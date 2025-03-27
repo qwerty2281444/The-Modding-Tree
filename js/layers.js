7a3b4cc6b4eb6593eb2e177ade61481cb459addd
qwerty2281444 <alihanryspek044@gmail.com>
2025-03-27 22:08:47 +0500

    e

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("w", {
     name: "weapons", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -66,7 +66,7 @@ addLayer("b", {
 		points: new Decimal(0),
     }},
     branches: ["w"],
-    row: 1,
+    row: 2,
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
