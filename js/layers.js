810af47520f3cbaa853c09312d86873bfec8824c
qwerty2281444 <alihanryspek044@gmail.com>
2025-03-27 13:52:27 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -31,6 +31,7 @@ addLayer("b", {
     symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
     branch: "prestige",
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    row: 2,
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -39,7 +40,7 @@ addLayer("b", {
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "booster points", // Name of prestige currency
     baseResource: "prestige points", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player.prestigepoints}, // Get the current amount of baseResource
     type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
