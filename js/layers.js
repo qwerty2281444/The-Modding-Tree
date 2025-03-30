5ebacb3f0ba39b7314d0c0188d90ef0b6f1d6f24
qwerty2281444 <alihanryspek044@gmail.com>
2025-03-30 22:36:14 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -54,3 +54,41 @@ addLayer("r", {
         },
     },
 })
+
+addLayer("b", {
+    name: "blue", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#0011ff",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "blue points", // Name of prestige currency
+    baseResource: "red points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    branch: ["r"],
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            title: "Blue Color",
+            description: "Double your point gain.",
+            cost: new Decimal(1),
+        },
+    },
+})
+
