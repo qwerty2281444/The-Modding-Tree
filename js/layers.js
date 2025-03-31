d62eb4713f08d26bdff3a6f0daf8b7c111af41a1
qwerty2281444 <alihanryspek044@gmail.com>
2025-03-31 15:43:19 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -100,3 +100,41 @@ addLayer("b", {
     },
 })
 
+addLayer("y", {
+    name: "yellow", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "Y", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#ffff00",
+    requires: new Decimal(10000), // Can be a function that takes requirement increases into account
+    resource: "yellow points", // Name of prestige currency
+    baseResource: "color", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    branches: ["r","b"],
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            title: "Yellow",
+            description: "double red points gain.",
+            cost: new Decimal(1),
+        },
+    },
+})
+
