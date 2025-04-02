edec2562594227b3d4d6b52e7c11d5d2fcf25884
qwerty2281444 <alihanryspek044@gmail.com>
2025-04-02 12:59:53 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -152,3 +152,58 @@ addLayer("y", {
     },
 })
 
+addLayer("g", {
+    name: "green", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "G", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#00ff00",
+    requires: new Decimal(10000), // Can be a function that takes requirement increases into account
+    resource: "green points", // Name of prestige currency
+    baseResource: "yellow points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.6, // Prestige currency exponent
+
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row)
+    branches: ["b","y"],
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    milestones: {
+        0: {
+            requirementDesc: "1 green points",
+            effectDesc: "keep red upgrades on blue and yellow resets",
+            done() { return player.g.points.gte(1) }
+        },
+        1: {
+            requirementDesc: "2 green points",
+            effectDesc: "generate 100% of red points on reset",
+            done() { return player.g.points.gte(2) }
+        },
+    }
+
+    // upgrades: {
+    //     11: {
+    //         title: "Yellow Color",
+    //         description: "Yellow points boost points at reduced rate.",
+    //         cost: new Decimal(1),
+    //         effect() {
+    //             return player.y.points.add(1).pow(0.1)
+    //         },
+    //         effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+    //     },
+    // },
+})
+
